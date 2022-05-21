482f9adca3d52d4fd61665abcb63bab476a86615
Devesh Kumar <devesh2027@gmail.com>
2022-05-21 17:46:22 +0530

    Updated comment in WrapperComponent

--- src/WrapperComponent.js
+++ src/WrapperComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const WrapperComponent = ({ Component, pageProps }) => {
 	// On server side: This component simply passes pageProps to the component
-	// On client-side: While rendering this component picks up the pageprops from <script id="isomorph-data"></script> and passes it to the component.
+	// On client-side: While rendering this component picks up the pageprops from <script type="isomorph/data"></script> and passes it to the component.
 	return <Component {...pageProps} />;
 };
 
