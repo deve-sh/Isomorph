592a848fd860248a67cebba211035d03096b6fda
Devesh Kumar <devesh2027@gmail.com>
2022-05-21 14:49:46 +0530

    Added handling of dynamic routes + 404 and 500

--- src/server.js
+++ src/server.js
@@ -2,31 +2,54 @@ import express from "express";
 import React from "react";
 import ReactDOM from "react-dom/server";
 import WrapperComponent from "./WrapperComponent";
-import * as ComponentExports from "./ReactComponent";
 
 const app = express();
 
+const nullFunction = () => ({});
+
 app.get("*", async (req, res) => {
-	const ComponentDefault = ComponentExports.default; // The React component
-	const getPropsFromServerForComponent = ComponentExports.getPropsOnServer;
-	let initialProps = {};
-	if (getPropsFromServerForComponent) {
-		initialProps =
-			(await getPropsFromServerForComponent({ url: req.url })) || {};
+	const pageRoute = req.url;
+	const pageImportPath = `./pages${
+		pageRoute.endsWith("/")
+			? pageRoute.slice(0, pageRoute.length - 1)
+			: pageRoute
+	}`;
+	let ComponentExports;
+	try {
+		ComponentExports = await import(pageImportPath);
+	} catch {
+		// Todo: Add _error or default component file for 404 errors.
+		return res.sendStatus(404);
 	}
-	const componentOutput = ReactDOM.renderToString(
-		<WrapperComponent Component={ComponentDefault} pageProps={initialProps} />
-	);
-	res.send(`
-	<html>
-		<head>
-			<title>App Rendered By Isomorph</title>
-			<script id="isomorph-data">${JSON.stringify(initialProps)}</script>
-		</head>
-		<body>
-			${componentOutput}
-		</body>
-	</html>`);
+	try {
+		const {
+			default: ComponentDefault, // The React component
+			getPropsFromServerForComponent = nullFunction,
+			getComponentMeta = nullFunction,
+		} = ComponentExports;
+		const context = { url: req.url, req, res };
+		let [initialProps, componentMeta] = await Promise.all([
+			getPropsFromServerForComponent(context),
+			getComponentMeta(context),
+		]);
+		const componentOutput = ReactDOM.renderToString(
+			<WrapperComponent Component={ComponentDefault} pageProps={initialProps} />
+		);
+		res.send(`
+			<html>
+				<head>
+					<title>${componentMeta?.title || "App Rendered By Isomorph"}</title>
+					<script id="isomorph-data">${JSON.stringify(initialProps)}</script>
+				</head>
+				<body>
+					${componentOutput}
+				</body>
+			</html>
+		`);
+	} catch {
+		// Todo: Add default _error component page for handling 500 errors too.
+		return res.sendStatus(500);
+	}
 });
 
 app.listen(5000, () => {
